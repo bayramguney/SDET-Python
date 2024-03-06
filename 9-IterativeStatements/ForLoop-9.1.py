7a023beb240bbd58e2a48265c216c15c400749c3
bayram <bayramguney@yahoo.com>
2024-03-06 17:45:30 -0500

    9.lesson added

---
+++ 9-IterativeStatements/ForLoop-9.1.py
@@ -0,0 +1,12 @@
+# print 1 to 10 numbers
+for i in range(10):
+    print(i)   # 0 1 2 3 4 5 6 7 8 9
+# print even numbers between 1  10
+for i in range(2,10,2):
+    print(i)   # 2 4 6 8
+# print odd numbers between 1  10
+for i in range(1,10,2):
+    print(i)   # 1 3 5 7 9
+# print numbers between 10  1 descending order
+for i in range(10,1,-1):
+    print(i)   # 10 9 8 7 6 5 4 3 2
---
+++ 9-IterativeStatements/WhileLoop-9.2.py
@@ -0,0 +1,9 @@
+i = 1
+while i <= 10:    # it will stop when it is FALSE
+    print(i)    # 1 2 3 4 5 6 7 8 9 10
+    i = i+1
+
+i = 10
+while i >= 1:    # it will stop when it is FALSE
+    print(i)    # 10 9 8 7 6 5 4 3 2 1
+    i = i-1
